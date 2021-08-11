c95cfd4127cd40a76f18784b7d5db0403ffe2994
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-08-11 18:06:48 +0700

    Update Login

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World Aws Ecs Go Go Go');
 })
 
-app.post('/', (req, res) => {
-    const {username, password} = req.body;
-    res.send(`Hello ${username} ${password}`);
-})
+app.post('/login',function(req,res){
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.end("yes" + password);
+    });
 
 app.listen(80);
