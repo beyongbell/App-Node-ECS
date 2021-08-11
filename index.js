c9253c7e9249ff1354fc226752fe89ab617f0764
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-08-11 15:34:21 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World Aws Ecs Test Load Balance');
+    res.send('Hello World Aws Ecs To Ec2');
 })
 
 app.listen(80);
