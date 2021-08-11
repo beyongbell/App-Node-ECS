b43bcadef24205a9475020c18779a28d3179db73
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-08-11 13:56:28 +0700

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World Aws');
+    res.send('Hello World Aws Ecs');
 })
 
 app.listen(80);
